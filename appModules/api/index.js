d9516e326ab0fd5cac3e35276e47e272b69f8f72
andreiusha228 <andreybukovsky08@gmail.com>
2024-04-28 13:37:13 +0200

    Второе ДЗ

---
+++ appModules/api/index.js
@@ -0,0 +1,8 @@
+const endpoints = require('./config.js');
+const { getData, getRandomGame } = require('./api-utils.js');
+
+module.exports = {
+    endpoints,
+    getData,
+    getRandomGame
+};
---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const mainRouteController = require("./main.js");
+const defaultRouteController = require("./default.js");
+const gameRouteController = require("./game.js");
+const voteRouteController = require ("./vote.js")
+
+
+module.exports = { 
+    mainRouteController, 
+    defaultRouteController, 
+    gameRouteController,
+    voteRouteController,
+};
